12e9b3166f38b5e5564ba13b6c897c59260dbb84
Oscar Galvez <oscrui@dexeus.com>
2020-01-22 16:52:41 +0100

    Ejemplo básico de lanzamiento de una excepción propia

---
+++ manejoExcepciones/manejoExcepciones.php
@@ -0,0 +1,19 @@
+<?php
+
+
+$miLado = -3;
+function areaCuadrado($lado){
+
+    if ($lado < 0){
+        // Lanzamos una excepción
+        throw new Exception ('Debes insertar un número positivo');
+    }
+    return $lado * $lado;
+}
+
+try {
+    areaCuadrado($miLado);
+} catch (Exception $e) {
+    print 'Ha saltado la excepción: ' . $e->getMessage();
+}
+// Devuelve: Uncaught exception 'Exception' with message 'Debes insertar un número positivo'
